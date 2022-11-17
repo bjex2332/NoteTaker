e8d71f69fcc1f762159d36c346659b2ec23ee1d0
Brandon <bjex2332@gmail.com>
2022-11-17 16:52:46 -0700

    Slight change to fs

--- Routes/notesRoutes.js
+++ Routes/notesRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile, deleteNote } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, deleteNote } = require('../helpers/fs');
 
 // route to get notes
 router.get('/api/notes', (req, res) => 
